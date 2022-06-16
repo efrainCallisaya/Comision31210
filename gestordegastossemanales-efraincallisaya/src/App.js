4bbec7dc2a3e1e2eea2d07130f4bc9b2476f96d8
efrainCallisaya <efra.man.calli.hino@gmail.com>
2022-06-16 18:03:03 -0300

    se genera nav con bootstrap

--- gestordegastossemanales-efraincallisaya/src/App.js
+++ gestordegastossemanales-efraincallisaya/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav class="navbar navbar-light bg-light static-top">
+    <div class="container ">
+        <a class="navbar-brand" href="">AtraPA2</a>
+       
+        <a href="./pages/comprasPrimavera.html">
+            <button type="button" class="btn btn-outline-success py-1 border-0">Primavera </button>
         </a>
-      </header>
+        <a href="./pages/comprasVerano.html">
+            <button type="button" class="btn btn-outline-danger py-1 border-0">Verano</button>
+        </a>
+        <a href="./pages/comprasOtonio.html">
+            <button type="button" class="btn btn-outline-dark py-1 border-0">Otoño</button>
+        </a>
+        <a href="./pages/comprasInvierno.html">
+            <button type="button" class="btn btn-outline-primary py-1 border-0">Invierno</button>
+        </a>
+        <button class="btn btn-outline-dark" type="submit">
+            <i class="bi-cart-fill me-1"></i>
+            carrito
+            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+            
+        </button>
+        <a class="btn btn-primary" href="">Iniciar Sesion</a>
+        
     </div>
-  );
+</nav>
+  )
 }
-
-export default App;
